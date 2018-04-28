4f6f74718285f8c53d55830d4aca6802c9ad29ef
IAmMonmoy <iammonmoy@gmail.com>
2018-04-28 11:26:41 +0600

    added frontend for loginform

--- AngularTokenAuthentication/src/app/authentication/authentication.module.ts
+++ AngularTokenAuthentication/src/app/authentication/authentication.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
@@ -8,7 +9,8 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginComponent, RegisterComponent]
 })
