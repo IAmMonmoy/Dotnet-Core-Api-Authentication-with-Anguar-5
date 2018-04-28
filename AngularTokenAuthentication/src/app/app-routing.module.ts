02c71c6a97fe8d4121c80cdeaab0bca733969439
IAmMonmoy <iammonmoy@gmail.com>
2018-04-28 10:18:16 +0600

    added navbar and update default routing path

--- AngularTokenAuthentication/src/app/app-routing.module.ts
+++ AngularTokenAuthentication/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, Router } from '@angular/router';
-import { LoginComponent } from './authentication/login/login.component';
-import { AppComponent } from './app.component';
+import { UserComponent } from './authorization/user/user.component';
 
 const routes : Routes = [
-  { path: '', component: AppComponent }
+  { path: '', component: UserComponent }
 ]
 
 @NgModule({
