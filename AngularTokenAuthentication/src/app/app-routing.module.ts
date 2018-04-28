3e65c6be219c2ebf2fa2229b4c9d3934b3d060aa
IAmMonmoy <iammonmoy@gmail.com>
2018-04-28 09:55:36 +0600

    added jquery fixed error in app routing module

--- AngularTokenAuthentication/src/app/app-routing.module.ts
+++ AngularTokenAuthentication/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { AppComponent } from './app.component';
 
 const routes : Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: AppComponent }
 ]
 
 @NgModule({
@@ -12,6 +13,8 @@ const routes : Routes = [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule]
+  ,
   declarations: []
 })
 export class AppRoutingModule { }
