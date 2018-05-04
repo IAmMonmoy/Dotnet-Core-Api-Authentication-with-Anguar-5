8b9a742747fc190c0f3446655745b11b9e8c35b9
IAmMonmoy <iammonmoy@gmail.com>
2018-05-04 10:53:28 +0600

    added authguard to user route

--- AngularTokenAuthentication/src/app/authorization/authorization-routing.module.ts
+++ AngularTokenAuthentication/src/app/authorization/authorization-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
+import { AuthGuardService } from '../authentication/services/auth-guard.service';
 
 const routes: Routes = [
-  { path: 'user', component: UserComponent },
+  { path: 'user', component: UserComponent, canActivate : [AuthGuardService] },
   { path: 'admin', component: AdminComponent }
 ];
 
--- AngularTokenAuthentication/src/app/authorization/authorization.module.ts
+++ AngularTokenAuthentication/src/app/authorization/authorization.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AuthorizationRoutingModule } from './authorization-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { UserComponent } from './user/user.component';
+import { AuthGuardService } from '../authentication/services/auth-guard.service';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthorizationRoutingModule
   ],
+  providers : [AuthGuardService]
+  ,
   declarations: [AdminComponent, UserComponent]
 })
 export class AuthorizationModule { }
